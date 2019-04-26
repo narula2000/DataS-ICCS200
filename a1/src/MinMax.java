fe559a0f0d875c337215741b0042b248d026005f
Vikrom Narula <djnarula@gmail.com>
2019-04-26 18:56:43 +0700

    Clean

--- a1/src/MinMax.java
+++ a1/src/MinMax.java
@@ -1,20 +1,20 @@
 public class MinMax {
-  public static double minMaxAverage(int[] numbers) {
-    // TODO: write code to find min and max as specified in the handout
-    int pair = numbers.length;
-    int[] maxList;
-    int[] minList;
-    if (pair%2==0) {
-      for (int i = 0; i < pair; i++){
-        if (numbers[i]>= numbers[i + pair])
-        {
-          maxList
-        }
-      }
-    }else {
+	public static double minMaxAverage(int[] numbers) {
+		// TODO: write code to find min and max as specified in the handout
+		int pair = numbers.length;
+		int[] maxList;
+		int[] minList;
+		if (pair%2==0) {
+			for (int i = 0; i < pair; i++){
+				if (numbers[i]>= numbers[i + pair])
+				{
+					maxList
+				}
+			}
+		}else {
 
-    }
-    int myMin = 0; // make it work
-    int myMax = 5; // make it work
-    return (myMin + myMax)/2.0; }
+		}
+		int myMin = 0; // make it work
+		int myMax = 5; // make it work
+		return (myMin + myMax)/2.0; }
 }
