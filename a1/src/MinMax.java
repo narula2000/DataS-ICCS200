9d85da87b96dc0fc53d0e34ab94a5b817b24b732
Vikrom Narula <djnarula@gmail.com>
2019-04-26 18:38:22 +0700

    Test push

--- a1/src/MinMax.java
+++ a1/src/MinMax.java
@@ -1,7 +1,19 @@
 public class MinMax {
   public static double minMaxAverage(int[] numbers) {
     // TODO: write code to find min and max as specified in the handout
-    // replace the following code with your implementation
+    int pair = numbers.length;
+    int[] maxList;
+    int[] minList;
+    if (pair%2==0) {
+      for (int i = 0; i < pair; i++){
+        if (numbers[i]>= numbers[i + pair])
+        {
+          maxList
+        }
+      }
+    }else {
+
+    }
     int myMin = 0; // make it work
     int myMax = 5; // make it work
     return (myMin + myMax)/2.0; }
