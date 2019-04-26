2571df9613d3589fd6c049863a3db606182a0862
Vikrom Narula <djnarula@gmail.com>
2019-04-26 19:34:01 +0700

    Finish With a1.1

--- a1/src/MinMax.java
+++ a1/src/MinMax.java
@@ -1,20 +1,51 @@
 public class MinMax {
-	public static double minMaxAverage(int[] numbers) {
-		// TODO: write code to find min and max as specified in the handout
-		int pair = numbers.length;
-		int[] maxList;
-		int[] minList;
-		if (pair%2==0) {
-			for (int i = 0; i < pair; i++){
-				if (numbers[i]>= numbers[i + pair])
-				{
-					maxList
-				}
-			}
-		}else {
-
-		}
-		int myMin = 0; // make it work
-		int myMax = 5; // make it work
-		return (myMin + myMax)/2.0; }
+    public static double minMaxAverage(int[] numbers) {
+        int pair = numbers.length;
+        int[] maxList;
+        int[] minList;
+        if (pair % 2 == 0) { // Even length
+            maxList = new int[pair / 2];
+            minList = new int[pair / 2];
+            for (int i = 0; i < pair; i++) {
+                if (numbers[i] >= numbers[i + pair]) {
+                    maxList[i] = numbers[i];
+                    minList[i] = numbers[i + pair];
+                }
+                if (numbers[i] <= numbers[i + pair]) {
+                    maxList[i] = numbers[i + pair];
+                    minList[i] = numbers[i];
+                }
+            }
+        } else { // Odd length
+            maxList = new int[(pair / 2) + 1];
+            minList = new int[(pair / 2) + 1];
+            for (int i = 0; i < pair; i++) {
+                if (numbers[i] >= numbers[i + pair]) {
+                    maxList[i] = numbers[i];
+                    minList[i] = numbers[i + pair];
+                }
+                if (numbers[i] <= numbers[i + pair]) {
+                    maxList[i] = numbers[i + pair];
+                    minList[i] = numbers[i];
+                }
+            }
+            maxList[maxList.length-1] = numbers[numbers.length-1];
+            minList[minList.length-1] = numbers[numbers.length-1];
+        }
+        int myMin = minList[0]; // Min to the first index
+        int myMax = maxList[0]; // Max to the first index
+        // Max finder
+        for (int value : maxList) {
+            if (myMax < value) {
+                myMax = value;
+            }
+        }
+        // Min finder
+        for (int value : minList) {
+            if (myMin > value) {
+                myMin = value;
+            }
+        }
+        return (myMin + myMax) / 2.0;
+    }
 }
