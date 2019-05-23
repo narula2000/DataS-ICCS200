892f9bf727f535863b98887c88fe0c9285eb10de
narula2000 <djnarula@gmail.com>
2019-05-23 12:26:23 +0700

    Modification

--- a3/src/BombremanHackerank.java
+++ a3/src/BombremanHackerank.java
@@ -1,86 +1,86 @@
 public class BombremanHackerank {
-    private static void filler(String[] grid) {
-        for (int i = 0; i < grid.length; i++) {
-            grid[i] = grid[i].replace(".", "X");
-        }
-    }
+	private static void filler(String[] grid) {
+		for (int i = 0; i < grid.length; i++) {
+			grid[i] = grid[i].replace(".", "X");
+		}
+	}
 
-    private static void explode(String[] grid, String[] oriGrid) {
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length(); j++) {
-                if (oriGrid[i].charAt(j) == 'O') {
-                    if (j == 0) {
-                        grid[i] = '.' + grid[i].substring(1);
-                    }
-                    if (j == grid[i].length() - 1) {
-                        grid[i] = grid[i].substring(0, j) + '.';
-                    }
-                    if (j > 0) {
-                        grid[i] = grid[i].substring(0, j - 1) + '.' + grid[i].substring(j); // Left
-                    }
-                    if (j < grid[i].length() - 1) {
-                        grid[i] = grid[i].substring(0, j + 1) + '.' + grid[i].substring(j + 2); // Right
-                    }
-                    if (i < grid.length - 1) {
-                        grid[i + 1] = grid[i + 1].substring(0, j) + '.' + grid[i + 1].substring(j + 1); // Bottom
-                    }
-                    if (i > 0) {
-                        grid[i - 1] = grid[i - 1].substring(0, j) + '.' + grid[i - 1].substring(j + 1); // Top
-                    }
-                    if (j != 0 || j != grid[i].length()) {
-                        grid[i] = grid[i].substring(0, j) + '.' + grid[i].substring(j + 1);
-                    }
-                }
-            }
-        }
-    }
+	private static void explode(String[] grid, String[] oriGrid) {
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[0].length(); j++) {
+				if (oriGrid[i].charAt(j) == 'O') {
+					if (j == 0) {
+						grid[i] = '.' + grid[i].substring(1);
+					}
+					if (j == grid[i].length() - 1) {
+						grid[i] = grid[i].substring(0, j) + '.';
+					}
+					if (j > 0) {
+						grid[i] = grid[i].substring(0, j - 1) + '.' + grid[i].substring(j); // Left
+					}
+					if (j < grid[i].length() - 1) {
+						grid[i] = grid[i].substring(0, j + 1) + '.' + grid[i].substring(j + 2); // Right
+					}
+					if (i < grid.length - 1) {
+						grid[i + 1] = grid[i + 1].substring(0, j) + '.' + grid[i + 1].substring(j + 1); // Bottom
+					}
+					if (i > 0) {
+						grid[i - 1] = grid[i - 1].substring(0, j) + '.' + grid[i - 1].substring(j + 1); // Top
+					}
+					if (j != 0 || j != grid[i].length()) {
+						grid[i] = grid[i].substring(0, j) + '.' + grid[i].substring(j + 1);
+					}
+				}
+			}
+		}
+	}
 
-    private static void rearrange(String[] grid) {
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length(); j++) {
-                grid[i] = grid[i].replace('X', 'O');
-            }
-        }
-    }
+	private static void rearrange(String[] grid) {
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[0].length(); j++) {
+				grid[i] = grid[i].replace('X', 'O');
+			}
+		}
+	}
 
-    static String[] bomberMan(int n, String[] grid) {
-        int column = grid[0].length();
-        int row = grid.length;
-        String[] A = new String[grid.length];
-        for (int index = 0; index < grid.length; index++)
-            A[index] = grid[index];
-        if (n == 1)
-            return grid;
-        if (n % 2 == 0) {
-            for (int i = 0; i < row; i++) {
-                for (int j = 0; j < column; j++) {
-                    A[i] = A[i].replace('.', 'O');
-                }
-            }
-            return A;
-        }
-        // Make 3 to A and 5 to B
-        filler(A);
-        explode(A, grid);
-        rearrange(A);
-        while (n > 7) {
-            n -= 4;
-        }
-        if (n == 3)
-            return A;
-        else {
-            String[] B = new String[grid.length];
-            for (int index = 0; index < grid.length; index++)
-                B[index] = A[index];
-            filler(B);
-            explode(B, A);
-            rearrange(B);
-            return B;
-        }
-    }
+	static String[] bomberMan(int n, String[] grid) {
+		int column = grid[0].length();
+		int row = grid.length;
+		String[] A = new String[grid.length];
+		for (int index = 0; index < grid.length; index++)
+			A[index] = grid[index];
+		if (n == 1)
+			return grid;
+		if (n % 2 == 0) {
+			for (int i = 0; i < row; i++) {
+				for (int j = 0; j < column; j++) {
+					A[i] = A[i].replace('.', 'O');
+				}
+			}
+			return A;
+		}
+		// Make 3 to A and 5 to B
+		filler(A);
+		explode(A, grid);
+		rearrange(A);
+		while (n > 7) {
+			n -= 4;
+		}
+		if (n == 3)
+			return A;
+		else {
+			String[] B = new String[grid.length];
+			for (int index = 0; index < grid.length; index++)
+				B[index] = A[index];
+			filler(B);
+			explode(B, A);
+			rearrange(B);
+			return B;
+		}
+	}
 
-    public static void main(String[] args) {
-        String[] A = new String[] { ".......", "...O...", "....O..", ".......", "OO.....", "OO....." };
-        bomberMan(3, A);
-    }
+	public static void main(String[] args) {
+		String[] A = new String[] { ".......", "...O...", "....O..", ".......", "OO.....", "OO....." };
+		System.out.println(bomberMan(3, A));
+	}
 }
