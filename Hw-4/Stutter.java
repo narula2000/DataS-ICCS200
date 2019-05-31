d71f458f3769659a52ee413217e3345824905c63
narula2000 <djnarula@gmail.com>
2019-05-31 22:54:02 +0700

    Added Comments

--- Hw-4/Stutter.java
+++ Hw-4/Stutter.java
@@ -1,23 +1,22 @@
 public class Stutter {
-    //collaborator: Ink-go, Harvey, Intellij_คริคริ
-    public static boolean isSubstr(String a, String b) {
+    public static boolean isSubstr(String test, String tester) {
         int counter = 0;
-        for (int i = 0; i < b.length(); i++) { //run from i=0 to b.length-()1 in the worst case scenario
-            if (a.charAt(counter) == b.charAt(i)) { //takes O(1)
+        for (int i = 0; i < tester.length(); i++) {
+            if (test.charAt(counter) == tester.charAt(i)) {
                 counter++;
             }
-            if (a.length() == counter) { //O(1)
+            if (test.length() == counter) {
                 return true;
             }
         }
         return false;
-    } //the running time is O(b.length())
+    }
 
-    public static String stutter(String A, int k) {
-        StringBuilder stuttered = new StringBuilder(""); //I dont know why the hell my dear collaborator Intellij prefers that I use StringBuilder.....
+    public static String stutter(String A, int k) { // Make a stutter String
+        StringBuilder stuttered = new StringBuilder("");
         for (int i = 0; i < A.length(); i++) {
             for (int j = 0; j < k; j++) {
-                stuttered.append(A.charAt(i)); /// here is where it got complained it doesnt want me to do stuttered +=
+                stuttered.append(A.charAt(i));
             }
         }
         return stuttered.toString();
@@ -25,32 +24,22 @@ public class Stutter {
 
     public static int maxStutter(String a, String b) {
 
-
         int m = b.length();
         int n = a.length();
-        int max = m / n;
+        int max = m / n; // Most possible substring it can has
 
-        return helperMaxStutter(a, b, 0, max);
+        return helperMS(a, b, 0, max);
 
     }
 
-    //    public static int helperMaxStutter(String a, String b,int check, int whereAmI ){
-    public static int helperMaxStutter(String a, String b, int low, int up) {
-        if (low - up >= -1) return low;
+    public static int helperMS(String a, String b, int low, int up) {
+        if (low - up >= -1)
+            return low; // If A > B return 0 also keep tracks of how many iteration it has gone
         else {
-//            int time = 1;System.out.println(time); time++;
-            if (isSubstr(stutter(a, (low + up) / 2), b)) {
-                return helperMaxStutter(a, b, (low + up) / 2, up);
-            } else return helperMaxStutter(a, b, low, (low + up) / 2);
+            if (isSubstr(stutter(a, (low + up) / 2), b)) { // Check if stutter String in is Sub
+                return helperMS(a, b, (low + up) / 2, up); // Increase k by half of max
+            } else
+                return helperMS(a, b, low, (low + up) / 2); // Decrease k by half of max
         }
     }
-
-    public static void main(String[] args) {
-        String s = "assadfgfdhjukiluiluilf", stuttthis = "Kanat", a = "ho", b = "hi and hhhhhoeololo jello";
-        Stutter stutter = new Stutter();
-        System.out.println(stutter.isSubstr("aiif", s));
-        System.out.println(stutter.stutter(stuttthis, 3));
-        System.out.println(stutter.maxStutter(a, b));
-    }
-
 }
