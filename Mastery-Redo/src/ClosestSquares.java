4ab1891bc79bdc17b64b228743a5b9df75025885
narula2000 <djnarula@gmail.com>
2019-06-14 11:46:31 +0700

    Redo in Atom

--- Mastery-Redo/src/ClosestSquares.java
+++ Mastery-Redo/src/ClosestSquares.java
@@ -2,35 +2,36 @@ import java.util.Arrays;
 
 public class ClosestSquares {
 
-    public static int closestSquares(int[] a) {
-        long ans = Integer.MAX_VALUE;
-        long diff;
-        for(int i=0; i<a.length-1;i++){
-        if(a[i] <0) a[i] =-a[i];
-        }
-        Arrays.sort(a);
-        for (int i = 0; i < a.length - 1; i++) {
-            diff = diffsquared(a[i] * a[i], a[i + 1] * a[i + 1]);
-            if (diff < ans)
-                ans = diff;
-        }
-        return(int)ans;
-    }
+	public static int closestSquares(int[] a) {
+		long ans = Integer.MAX_VALUE;
+		long diff;
+		for (int i = 0; i < a.length - 1; i++) {
+			if (a[i] < 0) a[i] = -a[i];
+		}
+		Arrays.sort(a);
+		for (int i = 0; i < a.length - 1; i++) {
+			diff = diffSquared(a[i] * a[i], a[i + 1] * a[i + 1]);
+			if (diff < ans)
+				ans = diff;
+		}
+		return (int) ans;
+	}
 
-    public static long diffsquared( long a,long b){
-        long diff;
-        if(a>b){
-         diff = a-b;}
-        else{
-            diff = b-a;
-        }
-        return diff;
-    }
-    public static void main(String[] args) {
-        int[] test0 = {-20,-3916237, -357920, -3620601,
-                7374819, -7330761, 30, 6246457, -6461594, 266854};
-        int expected = 500;
+	public static long diffSquared(long a, long b) {
+		long diff;
+		if (a > b) {
+			diff = a - b;
+		} else {
+			diff = b - a;
+		}
+		return diff;
+	}
 
-        System.out.println("verdict: "+(expected==closestSquares(test0)));
-    }
+	public static void main(String[] args) {
+		int[] test0 = {-20, -3916237, -357920, -3620601,
+				7374819, -7330761, 30, 6246457, -6461594, 266854};
+		int expected = 500;
+
+		System.out.println("verdict: " + (expected == closestSquares(test0)));
+	}
 }
--- Mastery-Redo/src/Weave.java
+++ Mastery-Redo/src/Weave.java
@@ -1,30 +1,23 @@
 public class Weave {
-    public static int numWeaves(int n) { //3-4-11
-        if (n ==0 ){
-            return 1;
-        }
-        else if(n==3){
-            return 1;
-        }
-        else if(n==4){
-            return 1;
-        }
-        else if (n == 1||n==2||n<0) {
-            return 0;
-        }
+	public static int numWeaves(int n) { //3-4-11
+		if (n == 0) {
+			return 1;
+		} else if (n == 3) {
+			return 1;
+		} else if (n == 4) {
+			return 1;
+		} else if (n == 1 || n == 2 || n < 0) {
+			return 0;
+		} else return numWeaves(n - 3) + numWeaves(n - 4) + numWeaves(n - 11);
+	}
 
-        else return numWeaves(n - 3) + numWeaves(n - 4) + numWeaves(n - 11);
+	public static void main(String[] args) {
+		int[] input = {3, 6, 14, 75};
+		int[] expected = {1, 1, 8, 5217077};
 
-
-    }
-
-    public static void main(String[] args) {
-        int[] input = {3, 6, 14, 75};
-        int[] expected = {1, 1, 8, 5217077};
-
-        for (int i = 0; i < input.length; i++) {
-            System.out.printf("Test #%d, verdict: %b [expected=%d]\n",
-                    i + 1, expected[i] == numWeaves(input[i]), expected[i]);
-        }
-    }
+		for (int i = 0; i < input.length; i++) {
+			System.out.printf("Test #%d, verdict: %b [expected=%d]\n",
+					i + 1, expected[i] == numWeaves(input[i]), expected[i]);
+		}
+	}
 }
