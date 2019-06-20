7178cde92d32e0ce1a28348298f0092d149d029c
narula2000 <djnarula@gmail.com>
2019-06-20 15:47:04 +0700

    Did a6 Exer 2

--- a6/src/MakeTree.java
+++ a6/src/MakeTree.java
@@ -1,43 +1,59 @@
 import java.util.Arrays;
 
 public class MakeTree {
-	public static BinaryTreeNode buildBST(int[] keys){
+	public static BinaryTreeNode buildBST(int[] keys) {
 		Arrays.sort(keys); // O( n * log n )
-		BinaryTreeNode tree = new BinaryTreeNode(keys[keys.length/2]);
-		 treeMaker(keys,0,keys.length/2 - 1,keys.length/2 + 1,keys.length-1, tree);
-	}
-	public static BinaryTreeNode treeMaker(int[] keys, int lowL, int highL, BinaryTreeNode T){
-		if (lowL > highL) return T;
-		else{
-		int mediumL = (lowL + highL)/2;
-		T.left.key = keys[mediumL];
-		treeMaker(keys,lowL,mediumL-1,mediumL+1,highL,T.left);
-		return T;
+		int medium = keys.length / 2;
+		return new BinaryTreeNode(
+				treeMaker(keys, 0, medium - 1),
+				keys[medium],
+				treeMaker(keys, medium + 1, keys.length - 1)
+		);
 	}
 
-	public static BinaryTreeNode treeMaker(int[] keys, int lowL, int highL, int lowR, int highR, BinaryTreeNode T){
-		if (lowL > highL) return T;
-		if (lowR > highR) return T;
-		else{
-			int mediumL = (lowL + highL)/2;
-			int mediumR = (lowR + highR)/2;
-			T.left.key = keys[mediumL];
-			T.right.key = keys[mediumR];
-			treeMaker(keys,lowL,mediumL-1,mediumL+1,highL,T.left);
-			treeMaker(keys,lowR,mediumR-1,mediumR+1,highR,T.right);}
-		return T;
+	public static BinaryTreeNode treeMaker(int[] keys, int low, int high) {
+		if (low == high) return new BinaryTreeNode(keys[(low + high) / 2]);
+		else {
+			int medium = (low + high) / 2;
+			if (low > medium-1) return new BinaryTreeNode(
+					null,
+					keys[medium],
+					treeMaker(keys, medium + 1, high));
+
+			if (medium + 1 > high ) return new BinaryTreeNode(
+					treeMaker(keys, low, medium - 1),
+					keys[medium],
+					null);
+
+			return new BinaryTreeNode(
+					treeMaker(keys, low, medium - 1),
+					keys[medium],
+					treeMaker(keys, medium + 1, high));
+		}
 	}
+
+
 	public static void main(String[] args) {
-		int[] keys = new int[9];
-		keys[0] = 1;
+		int[] keys = new int[10];
+		keys[0] = 13;
 		keys[1] = 2;
-		keys[2] = 3;
-		keys[3] = 4;
-		keys[4] = 5;
-		keys[5] = 6;
-		keys[6] = 7;
-		keys[7] = 8;
-		keys[8] = 9;
-		System.out.println(buildBST(keys));
+		keys[2] = 55;
+		keys[3] = 98;
+		keys[4] = 57;
+		keys[5] = 33;
+		keys[6] = 87;
+		keys[7] = 43;
+		keys[8] = 7;
+		keys[9] = 10;
+		printInOrder(buildBST(keys));
+	}
+
+
+
+	static void printInOrder(BinaryTreeNode T){
+		if (T==null) return;
+		printInOrder(T.left);
+		System.out.print(T.key + " ");
+		printInOrder(T.right);
 	}
 }
